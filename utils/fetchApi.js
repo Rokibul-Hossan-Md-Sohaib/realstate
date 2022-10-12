170bb14749db922e83f8ba81738af8f427486923
Rokibul-Hossan-Md-Sohaib <rokibulhossan32@gmail.com>
2022-10-12 18:40:07 +0600

    project done with next

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,9 +6,14 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      'X-RapidAPI-Key': '441dc5d9d3msh76757acd1fcb0dcp1e7488jsn1e6b6b9edb78',
     },
   });
     
   return data;
 }
+
+// headers: {
+//   'X-RapidAPI-Key': '441dc5d9d3msh76757acd1fcb0dcp1e7488jsn1e6b6b9edb78',
+//   'X-RapidAPI-Host': ''
+// }
